7fdb319bdc407159612dc9a8fda8da86a5faa674
Chengcheng Zhang <c845zhan@uwaterloo.ca>
2026-01-26 21:31:40 -0500

    Add drf-spectacular integration and update requirements

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "user",
 ]
 
@@ -129,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# drf-spectacular settings
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
