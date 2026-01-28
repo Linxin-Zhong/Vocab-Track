536ee189cddbaa3a7865a68e9a2adfd52801cd52
Chengcheng Zhang <AC-Dawn@users.noreply.github.com>
2026-01-28 09:42:49 -0500

    Uncomment API schema path in urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -26,7 +26,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls")),
-    # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
